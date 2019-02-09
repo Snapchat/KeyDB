f1fc8edb77c5bf22a81380b6c2d9805d68c479f1
John Sully <john@csquare.ca>
2019-02-09 13:05:06 -0500

    Performance enhancements!
    Former-commit-id: 7175a8e4ebee74233702b8e308c0d57661b54bac

--- deps/hiredis/sds.h
+++ deps/hiredis/sds.h
@@ -85,7 +85,8 @@ struct __attribute__ ((__packed__)) sdshdr64 {
 
 static inline size_t sdslen(const sds s) {
     unsigned char flags = s[-1];
-    switch(flags&SDS_TYPE_MASK) {
+    
+    switch(__builtin_expect((flags&SDS_TYPE_MASK), SDS_TYPE_5)) {
         case SDS_TYPE_5:
             return SDS_TYPE_5_LEN(flags);
         case SDS_TYPE_8:

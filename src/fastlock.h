988ea4010135a8aba106c9039f2dd46d916cbe58
John Sully <john@csquare.ca>
2019-02-10 22:00:19 -0500

    Fastlock fixes

--- src/fastlock.h
+++ src/fastlock.h
@@ -4,16 +4,31 @@
 extern "C" {
 #endif
 
-struct fastlock
-{
-    int lock;
-};
-
+/* Begin C API */
+struct fastlock;
 void fastlock_init(struct fastlock *lock);
 void fastlock_lock(struct fastlock *lock);
 void fastlock_unlock(struct fastlock *lock);
 void fastlock_free(struct fastlock *lock);
 
+/* End C API */
 #ifdef __cplusplus
 }
 #endif
+
+struct fastlock
+{
+    int m_lock;
+
+#ifdef __cplusplus
+    void lock()
+    {
+        fastlock_lock(this);
+    }
+
+    void unlock()
+    {
+        fastlock_unlock(this);
+    }
+#endif
+};

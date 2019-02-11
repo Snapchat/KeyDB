eefa3e91684c1d3b432eb435c9df0ebf02321794
John Sully <john@csquare.ca>
2019-02-10 22:00:19 -0500

    Fastlock fixes

--- src/fastlock.cpp
+++ src/fastlock.cpp
@@ -1,19 +1,23 @@
 #include "fastlock.h"
+#include <unistd.h>
 
 extern "C" void fastlock_init(struct fastlock *lock)
 {
-    lock->lock = 0;
+    lock->m_lock = 0;
 }
 
 extern "C" void fastlock_lock(struct fastlock *lock)
 {
-    while (!__sync_bool_compare_and_swap(&lock->lock, 0, 1));
+    while (!__sync_bool_compare_and_swap(&lock->m_lock, 0, 1))
+    {
+    }
 }
 
 extern "C" void fastlock_unlock(struct fastlock *lock)
 {
-    lock->lock = 0;
+    __sync_bool_compare_and_swap(&lock->m_lock, 1, 0);
 }
+
 extern "C" void fastlock_free(struct fastlock *lock)
 {
     // NOP

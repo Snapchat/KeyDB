83a7f2f2bd79459d5fa72b2cedf3b0cdfdb22123
John Sully <john@csquare.ca>
2019-04-07 16:54:56 -0400

    Move a bunch of files to C++
    Former-commit-id: 011948bcef748df33cb18e76dc5ba48d3c5706dc

--- src/pqsort.h
+++ src/pqsort.h
@@ -33,7 +33,7 @@
 #ifndef __PQSORT_H
 #define __PQSORT_H
 
-void
+extern "C" void
 pqsort(void *a, size_t n, size_t es,
     int (*cmp) (const void *, const void *), size_t lrange, size_t rrange);
 

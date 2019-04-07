9f95d8b38f837097f5041086e4af6fc8dcb9bd78
John Sully <john@csquare.ca>
2019-04-07 15:24:23 -0400

    Make t_string C++

--- src/server.h
+++ src/server.h
@@ -2033,7 +2033,7 @@ int listMatchPubsubPattern(void *a, void *b);
 int pubsubPublishMessage(robj *channel, robj *message);
 
 /* Keyspace events notification */
-void notifyKeyspaceEvent(int type, char *event, robj *key, int dbid);
+void notifyKeyspaceEvent(int type, const char *event, robj *key, int dbid);
 int keyspaceEventsStringToFlags(char *classes);
 sds keyspaceEventsFlagsToString(int flags);
 

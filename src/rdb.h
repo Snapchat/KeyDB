c07aa5f3fe4c614b5d97bc8a74e2f90064c066e1
John Sully <john@csquare.ca>
2019-04-19 22:54:42 -0400

    Additional MVCC work and fix memory leak loading objects from rdb
    Former-commit-id: efde2e6be6dc2fc3425a17e2dc146c5b8823730a

--- src/rdb.h
+++ src/rdb.h
@@ -146,7 +146,7 @@ int rdbSaveS3(char *path, rdbSaveInfo *rsi);
 int rdbLoadS3(char *path, rdbSaveInfo *rsi);
 ssize_t rdbSaveObject(rio *rdb, robj_roptr o, robj *key);
 size_t rdbSavedObjectLen(robj *o);
-robj *rdbLoadObject(int type, rio *rdb, robj *key);
+robj *rdbLoadObject(int type, rio *rdb, robj *key, uint64_t mvcc_tstamp);
 void backgroundSaveDoneHandler(int exitcode, int bysignal);
 int rdbSaveKeyValuePair(rio *rdb, robj *key, robj *val, long long expiretime);
 robj *rdbLoadStringObject(rio *rdb);

7896ca6e52b9caa9d8ab78145f16872fe488c4bb
John Sully <john@csquare.ca>
2019-05-09 19:05:09 -0400

    Add missing file

---
+++ src/new.h
@@ -0,0 +1,22 @@
+#pragma once
+
+[[deprecated]]
+inline void *operator new(size_t size)
+{
+    return zmalloc(size, MALLOC_LOCAL);
+}
+
+inline void *operator new(size_t size, enum MALLOC_CLASS mclass) 
+{ 
+    return zmalloc(size, mclass);
+} 
+
+inline void operator delete(void * p)
+{
+    zfree(p);
+}
+
+inline void operator delete(void *p, std::size_t)
+{
+    zfree(p);
+}

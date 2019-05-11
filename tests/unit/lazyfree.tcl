e601bc11697a94f5266a1c2c9ec323850c235ae3
John Sully <john@csquare.ca>
2019-05-11 17:57:35 -0400

    Make lazy free test more reliable
    Former-commit-id: 54d6eb851bbea48e858d84bca818cb14c65b6403

--- tests/unit/lazyfree.tcl
+++ tests/unit/lazyfree.tcl
@@ -2,11 +2,11 @@ start_server {tags {"lazyfree"}} {
     test "UNLINK can reclaim memory in background" {
         set orig_mem [s used_memory]
         set args {}
-        for {set i 0} {$i < 100000} {incr i} {
+        for {set i 0} {$i < 200000} {incr i} {
             lappend args $i
         }
         r sadd myset {*}$args
-        assert {[r scard myset] == 100000}
+        assert {[r scard myset] == 200000}
         set peak_mem [s used_memory]
         assert {[r unlink myset] == 1}
         assert {$peak_mem > $orig_mem+1000000}

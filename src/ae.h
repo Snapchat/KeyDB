0e45882715a9fc09bde68ca675bf0019ecf17797
John Sully <john@csquare.ca>
2019-06-18 00:06:42 -0400

    Fix libcxxabi clang compile failure in travis, as well as bool issue due to old clang
    Former-commit-id: 962196252e0c270997de26005705a2f8bbb82aa7

--- src/ae.h
+++ src/ae.h
@@ -159,7 +159,7 @@ int aeGetSetSize(aeEventLoop *eventLoop);
 int aeResizeSetSize(aeEventLoop *eventLoop, int setsize);
 
 void aeAcquireLock();
-int aeTryAcquireLock(bool fWeak);
+int aeTryAcquireLock(int fWeak);
 void aeReleaseLock();
 int aeThreadOwnsLock();
 

95f0357746cec6c36c54c50d968cb21e116e3e69
John Sully <john@csquare.ca>
2019-07-05 23:42:39 -0400

    Fix additional warning related to new() overloads
    Former-commit-id: 218111eb7244643bd13af47b878622c429535288

--- src/new.h
+++ src/new.h
@@ -4,14 +4,7 @@
 [[deprecated]]
 void *operator new(size_t size);
 
-inline void *operator new(size_t size, enum MALLOC_CLASS mclass) 
-{ 
-    return zmalloc(size, mclass);
-}
+void *operator new(size_t size, enum MALLOC_CLASS mclass);
 
 void operator delete(void * p) noexcept;
-
-inline void operator delete(void *p, std::size_t) noexcept
-{
-    zfree(p);
-}
+void operator delete(void *p, std::size_t) noexcept;

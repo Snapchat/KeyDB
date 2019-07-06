218111eb7244643bd13af47b878622c429535288
John Sully <john@csquare.ca>
2019-07-05 23:42:39 -0400

    Fix additional warning related to new() overloads

--- src/new.cpp
+++ src/new.cpp
@@ -8,8 +8,17 @@ void *operator new(size_t size)
     return zmalloc(size, MALLOC_LOCAL);
 }
 
+void *operator new(size_t size, enum MALLOC_CLASS mclass) 
+{ 
+    return zmalloc(size, mclass);
+}
+
 void operator delete(void * p) noexcept
 {
     zfree(p);
 }
 
+void operator delete(void *p, std::size_t) noexcept
+{
+    zfree(p);
+}

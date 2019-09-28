b01cf739999682182fef0dca9a05bdc5131d3fad
John Sully <john@csquare.ca>
2019-09-28 17:04:08 -0400

    RDB fuzz test cases
    Former-commit-id: 6df4d294220ed914c592f3bd195ae921d6a84cde

--- src/server.cpp
+++ src/server.cpp
@@ -5243,6 +5243,10 @@ int main(int argc, char **argv) {
     aeReleaseLock();    //Finally we can dump the lock
     moduleReleaseGIL(true);
 
+    // If we're just fuzzing then we've already loaded the RDB so just quit successfully
+    if (strstr(argv[0],"keydb-fuzz-rdb") != NULL)
+        return EXIT_SUCCESS;
+
     serverAssert(cserver.cthreads > 0 && cserver.cthreads <= MAX_EVENT_LOOPS);
     pthread_t rgthread[MAX_EVENT_LOOPS];
     for (int iel = 0; iel < cserver.cthreads; ++iel)

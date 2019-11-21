cff0abf580f13b7e451dc5a007e24d04eb9c8bb7
John Sully <john@csquare.ca>
2019-11-20 19:44:31 -0500

    Fix issue #107, active replicas do their own expires
    Former-commit-id: f6af4196efcebb1bdf8e7bfb2625b97158aed993

--- src/expire.cpp
+++ src/expire.cpp
@@ -535,7 +535,7 @@ void expireGenericCommand(client *c, long long basetime, int unit) {
      *
      * Instead we take the other branch of the IF statement setting an expire
      * (possibly in the past) and wait for an explicit DEL from the master. */
-    if (when <= mstime() && !g_pserver->loading && !listLength(g_pserver->masters)) {
+    if (when <= mstime() && !g_pserver->loading && (!listLength(g_pserver->masters) || g_pserver->fActiveReplica)) {
         robj *aux;
 
         int deleted = g_pserver->lazyfree_lazy_expire ? dbAsyncDelete(c->db,key) :

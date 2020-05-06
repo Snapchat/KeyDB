966b43659c91413a54ccae182e87b0f5167cca13
benschermel <ben@eqalpha.com>
2020-05-06 18:45:11 -0400

    add deb/rpm/docker support

--- pkg/rpm/generate_rpms.sh
+++ pkg/rpm/generate_rpms.sh
@@ -1,27 +1,24 @@
 #! /bin/bash
 
-### usage sudo ./generate_rpms [open source version] [build release #]
+### usage sudo ./generate_rpms
 
-version=$1
-release=$2
+version=$(grep KEYDB_REAL_VERSION ../../../src/version.h | awk '{ printf $3 }' | tr -d \")
+release=1 # by default this will always be 1 for keydb version structure. If build release version needs to be update you can modify here
 arch=$(uname -m)
 dist=el$(rpm -q --queryformat '%{VERSION}' centos-release | cut -d. -f1)
 
 if [ "$arch" != "aarch64" ] || [ "$arch" != "x86_64" ]; then
 	echo "This script is only valid and tested for aarch64 and x86_64 architectures. You are trying to use: $arch"
 fi
-if [ "$dist" != "el7" ] || [ "$dist" != "el8" ]; then
-        echo "This script is only valid and tested for centos7 and centos8 builds. You are trying to use: $dist"
-fi
 
 # remove any old rpm packages
 rm $PWD/rpm_files_generated/keydb*
 
 # generate empty directories that github would otherwise delete (avoids .gitkeep in directory)
-mkdir $PWD/centos7_rpm_build/keydb_build/keydb_rpm/usr/bin
-mkdir $PWD/centos7_rpm_build/keydb_build/keydb_rpm/usr/lib64/redis/modules
-mkdir $PWD/centos7_rpm_build/keydb_build/keydb_rpm/var/lib/keydb
-mkdir $PWD/centos7_rpm_build/keydb_build/keydb_rpm/var/log/keydb
+mkdir $PWD/keydb_build/keydb_rpm/usr/bin
+mkdir $PWD/keydb_build/keydb_rpm/usr/lib64/redis/modules
+mkdir $PWD/keydb_build/keydb_rpm/var/lib/keydb
+mkdir $PWD/keydb_build/keydb_rpm/var/log/keydb
 
 # move binaries to bin
 rm $PWD/keydb_build/keydb_rpm/usr/bin/*
@@ -34,8 +31,8 @@ sed -i '3d' $PWD/keydb_build/keydb.spec
 sed -i -E "2a\Release     : $release%{?dist}" $PWD/keydb_build/keydb.spec
 
 # yum install -y scl-utils centos-release-scl rpm-build
-mkdir -p /root/rpmbuild/BUILDROOT/keydb-$version5-$release.$dist.$arch
-cp -r ./keydb_build/keydb_rpm/* /root/rpmbuild/BUILDROOT/keydb-$version5-$release.$dist.$arch/
+mkdir -p /root/rpmbuild/BUILDROOT/keydb-$version-$release.$dist.$arch
+cp -r ./keydb_build/keydb_rpm/* /root/rpmbuild/BUILDROOT/keydb-$version-$release.$dist.$arch/
 rpmbuild -bb /rpm_build/keydb.spec
 mv /root/rpmbuild/RPMS/$arch/* .$PWD/rpm_files_generated
 

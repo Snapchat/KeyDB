2857bed8bdf68143043baa73a7bbd3dec31523aa
John Sully <john@csquare.ca>
2020-05-24 18:35:03 -0400

    bump version
    Former-commit-id: 9353b3aac2ab65120f2ad5caa2f8272ae02df380

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-#define KEYDB_REAL_VERSION "6.0.7"
+#define KEYDB_REAL_VERSION "6.0.8"
 extern const char *KEYDB_SET_VERSION;   // Unlike real version, this can be overriden by the config
 
 enum VersionCompareResult

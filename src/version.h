4d6b7c278b4ff796445d97777a674854d3b3180e
Ben Schermel <ben@eqalpha.com>
2020-06-16 14:17:20 -0400

    bump version after merge
    Former-commit-id: 59279d36e9337ca89518c4d1edef6c26911ea190

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-#define KEYDB_REAL_VERSION "6.0.10"
+#define KEYDB_REAL_VERSION "6.0.11"
 extern const char *KEYDB_SET_VERSION;   // Unlike real version, this can be overriden by the config
 
 enum VersionCompareResult

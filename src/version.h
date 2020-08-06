ef7198e24d7ecddcf7137a3ef1215317aa68c497
John Sully <john@csquare.ca>
2020-08-06 02:13:23 +0000

    keydbpro unstable version should be 0.0.0
    Former-commit-id: 4a6e0772f36d5dd721ad2e9b83f9fccce2e38ff0

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-#define KEYDB_REAL_VERSION "6.0.12"
+#define KEYDB_REAL_VERSION "0.0.0"
 extern const char *KEYDB_SET_VERSION;   // Unlike real version, this can be overriden by the config
 
 enum VersionCompareResult

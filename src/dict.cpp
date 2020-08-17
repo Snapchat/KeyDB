035cde502afbceff2bfec4602652b65881346a7f
John Sully <john@csquare.ca>
2020-08-17 00:33:37 +0000

    Fast cleanup of snapshots without leaving them forever
    Former-commit-id: fdd83b2b49244ed2988b080892ee5cffe9fd2684

--- src/dict.cpp
+++ src/dict.cpp
@@ -200,6 +200,8 @@ int dictMerge(dict *dst, dict *src)
         std::swap(dst->iterators, src->iterators);
     }
 
+    src->rehashidx = -1;
+
     if (!dictIsRehashing(dst) && !dictIsRehashing(src))
     {
         if (dst->ht[0].size >= src->ht[0].size)

188e7a5693386f47a7c462bffe6d9f5ed8090b9e
Kajaruban Surendran <kajaruban.surendran@huawei.com>
2020-11-27 21:42:08 +0000

    Configurable option for MOTD
    Former-commit-id: 49a89d636ba698dbd0858d5059d3d6387c8c1fc7

--- src/motd.h
+++ src/motd.h
@@ -6,8 +6,7 @@ extern const char *motd_cache_file;
 #ifdef __cplusplus
 extern "C" {
 #endif
-    
-char *fetchMOTD(int fCache);
+char *fetchMOTD(int fCache, int enable_motd);
 
 #ifdef __cplusplus
 }

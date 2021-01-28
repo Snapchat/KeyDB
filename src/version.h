595deb0e1391c29c4766072bdd292202dd55c79d
christianEQ <christian@eqalpha.com>
2021-01-28 16:54:22 +0000

    updated version num to 255.255.255

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-#define KEYDB_REAL_VERSION "0.0.0"
-#define KEYDB_VERSION_NUM 0x00000000
+#define KEYDB_REAL_VERSION "255.255.255"
+#define KEYDB_VERSION_NUM 0x00ffffff
 extern const char *KEYDB_SET_VERSION;   // Unlike real version, this can be overriden by the config
 

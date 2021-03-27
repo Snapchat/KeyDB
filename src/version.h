61d0605ae319cc009e6d212b42fd3a0236ea9199
benschermel <ben@eqalpha.com>
2021-03-27 18:26:11 +0000

    bump version
    Former-commit-id: 33b75e0a2cf8712c976e7af74e2a615bb991c356

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 #define KEYDB_REAL_VERSION "6.0.18"
-#define KEYDB_VERSION_NUM 0x00060011
+#define KEYDB_VERSION_NUM 0x00060012
 extern const char *KEYDB_SET_VERSION;   // Unlike real version, this can be overriden by the config
 
 enum VersionCompareResult

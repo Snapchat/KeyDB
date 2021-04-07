571718f774142e095be8caf24da3cc6efb22ad84
John Sully <john@csquare.ca>
2021-04-07 17:41:20 +0000

    Avoid posting unnecessary async tasks
    Former-commit-id: 08f63fdfd6c78388bba093ec2edda7d77fc9173e

--- src/server.cpp
+++ src/server.cpp
@@ -2441,13 +2441,15 @@ int serverCron(struct aeEventLoop *eventLoop, long long id, void *clientData) {
     ProcessPendingAsyncWrites();
 
     run_with_period(10) {
-        // Server threads don't free the GC, but if we don't have a
-        //  a bgsave or some other async task then we'll hold onto the
-        //  data for too long
-        g_pserver->asyncworkqueue->AddWorkFunction([]{
-            auto epoch = g_pserver->garbageCollector.startEpoch();
-            g_pserver->garbageCollector.endEpoch(epoch);
-        });
+        if (!g_pserver->garbageCollector.empty()) {
+            // Server threads don't free the GC, but if we don't have a
+            //  a bgsave or some other async task then we'll hold onto the
+            //  data for too long
+            g_pserver->asyncworkqueue->AddWorkFunction([]{
+                auto epoch = g_pserver->garbageCollector.startEpoch();
+                g_pserver->garbageCollector.endEpoch(epoch);
+            });
+        }
     }
 
     g_pserver->cronloops++;

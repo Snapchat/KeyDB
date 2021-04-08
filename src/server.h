69f7a194bc260111f02a05ed52ccd7ebf9fcccb5
John Sully <john@csquare.ca>
2021-04-08 19:52:38 +0000

    Make prefetch configurable
    Former-commit-id: 16996c048042bd3799c8051645bbe7c54137d54c

--- src/server.h
+++ src/server.h
@@ -2399,6 +2399,8 @@ struct redisServer {
     char *aof_rewrite_cpulist; /* cpu affinity list of aof rewrite process. */
     char *bgsave_cpulist; /* cpu affinity list of bgsave process. */
 
+    int prefetch_enabled = 1;
+
     long long repl_batch_offStart = -1;
     long long repl_batch_idxStart = -1;
 

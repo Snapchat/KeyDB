87add83a32094f965bf01cb1aa9f86183e5ac4f2
John Sully <john@csquare.ca>
2021-05-25 18:35:45 +0000

    More OS X build breaks
    Former-commit-id: 100a747eabf42bccbcf2991349be0fb3810799b6

--- src/redisassert.h
+++ src/redisassert.h
@@ -44,6 +44,7 @@
 extern "C" {
 #endif
 
+#undef assert
 #define assert(_e) (likely((_e))?(void)0 : (_serverAssert(#_e,__FILE__,__LINE__),redis_unreachable()))
 #define panic(...) _serverPanic(__FILE__,__LINE__,__VA_ARGS__),redis_unreachable()
 

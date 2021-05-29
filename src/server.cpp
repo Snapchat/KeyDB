d3c37c7159a92319759a33851669862a82cf1b28
John Sully <john@csquare.ca>
2021-05-29 01:19:12 +0000

    Fix failover command test failures

--- src/server.cpp
+++ src/server.cpp
@@ -2576,7 +2576,8 @@ void beforeSleep(struct aeEventLoop *eventLoop) {
     /* We may have recieved updates from clients about their current offset. NOTE:
      * this can't be done where the ACK is recieved since failover will disconnect 
      * our clients. */
-    updateFailoverStatus();
+    if (iel == IDX_EVENT_LOOP_MAIN)
+        updateFailoverStatus();
 
     /* Send the invalidation messages to clients participating to the
      * client side caching protocol in broadcasting (BCAST) mode. */
@@ -3426,7 +3427,6 @@ static void initServerThread(struct redisServerThreadVars *pvar, int fMain)
     pvar->cclients = 0;
     pvar->in_eval = 0;
     pvar->in_exec = 0;
-    pvar->client_pause_type = CLIENT_PAUSE_OFF;
     pvar->el = aeCreateEventLoop(g_pserver->maxclients+CONFIG_FDSET_INCR);
     aeSetBeforeSleepProc(pvar->el, beforeSleep, AE_SLEEP_THREADSAFE);
     aeSetAfterSleepProc(pvar->el, afterSleep, AE_SLEEP_THREADSAFE);
@@ -3518,6 +3518,7 @@ void initServer(void) {
     g_pserver->blocked_last_cron = 0;
     g_pserver->replication_allowed = 1;
     g_pserver->blocking_op_nesting = 0;
+    g_pserver->client_pause_type = CLIENT_PAUSE_OFF;
 
 
     if ((g_pserver->tls_port || g_pserver->tls_replication || g_pserver->tls_cluster)
@@ -4529,8 +4530,8 @@ int processCommand(client *c, int callFlags) {
     /* If the server is paused, block the client until
      * the pause has ended. Replicas are never paused. */
     if (!(c->flags & CLIENT_SLAVE) && 
-        ((serverTL->client_pause_type == CLIENT_PAUSE_ALL) ||
-        (serverTL->client_pause_type == CLIENT_PAUSE_WRITE && is_may_replicate_command)))
+        ((g_pserver->client_pause_type == CLIENT_PAUSE_ALL) ||
+        (g_pserver->client_pause_type == CLIENT_PAUSE_WRITE && is_may_replicate_command)))
     {
         c->bpop.timeout = 0;
         blockClient(c,BLOCKED_PAUSE);

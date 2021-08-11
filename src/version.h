aae676dc7d65df8f9e2f241a478be249e4ac422b
malavan <malavan@eqalpha.com>
2021-08-11 23:23:39 +0000

    bump version

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-#define KEYDB_REAL_VERSION "6.0.18"
-#define KEYDB_VERSION_NUM 0x00060012
+#define KEYDB_REAL_VERSION "6.2.0"
+#define KEYDB_VERSION_NUM 0x00060013
 extern const char *KEYDB_SET_VERSION;   // Unlike real version, this can be overriden by the config
 

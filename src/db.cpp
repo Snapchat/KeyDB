9ea336c4d311447544a3f5737e8a3c4814d3e0c9
John Sully <john@csquare.ca>
2021-09-23 17:44:06 +0000

    prefetch crashes intermittently when a snapshot exists
    Former-commit-id: 4a2657023d5d8218c815ce77c2676fd53a634b3f

--- src/db.cpp
+++ src/db.cpp
@@ -3160,6 +3160,8 @@ void redisDbPersistentData::prefetchKeysAsync(client *c, parsed_command &command
         if (command.argc >= 2) {
             const char *cmd = szFromObj(command.argv[0]);
             if (!strcasecmp(cmd, "set") || !strcasecmp(cmd, "get")) {
+                if (c->db->m_spdbSnapshotHOLDER != nullptr)
+                    return; // this is dangerous enough without a snapshot around
                 auto h = dictSdsHash(szFromObj(command.argv[1]));
                 for (int iht = 0; iht < 2; ++iht) {
                     auto hT = h & c->db->m_pdict->ht[iht].sizemask;

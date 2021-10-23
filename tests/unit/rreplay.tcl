c8c4828f8c642791d1f71f78a04f791556e593da
Eric de Ruiter <eric@thisisdevelopment.nl>
2021-10-23 11:48:57 +0200

    fix rreplay tests to also include allocate calls (this is needed as it uses debug force-master which causes the select to not allocate databases)

--- tests/unit/rreplay.tcl
+++ tests/unit/rreplay.tcl
@@ -2,6 +2,7 @@ start_server {tags {"rreplay"} overrides {active-replica yes}} {
 
     test {RREPLAY use current db} {
         r debug force-master yes
+        r allocate :: 4 0
         r select 4
         r set dbnum invalid
         r rreplay "f4d5b2b5-4f07-4ee5-a4f2-5dc98507dfce" "*3\r\n\$3\r\nSET\r\n\$5\r\ndbnum\r\n\$4\r\nfour\r\n"
@@ -11,6 +12,7 @@ start_server {tags {"rreplay"} overrides {active-replica yes}} {
 
     test {RREPLAY db different} {
         r debug force-master yes
+        r allocate :: 2 2
         r select 4
         r set testkey four
         r rreplay "f4d5b2b5-4f07-4ee5-a4f2-5dc98507dfce" "*3\r\n\$3\r\nSET\r\n\$7\r\ntestkey\r\n\$4\r\nbebe\r\n" 2

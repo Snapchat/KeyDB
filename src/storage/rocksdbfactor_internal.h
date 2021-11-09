8ea30a0e650c4f394da0e637d29701626692e139
John Sully <john@csquare.ca>
2021-11-09 19:36:07 +0000

    Initial implementation of snapshot fast replication.  There are still a few TODOs in progress
    Former-commit-id: 0febdcdab8693af443f350968ed3d8c80106675d

---
+++ src/storage/rocksdbfactor_internal.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "rocksdb.h"
+
+class RocksDBStorageFactory : public IStorageFactory
+{
+    std::shared_ptr<rocksdb::DB> m_spdb;    // Note: This must be first so it is deleted last
+    std::vector<std::unique_ptr<rocksdb::ColumnFamilyHandle>> m_vecspcols;
+    std::shared_ptr<rocksdb::SstFileManager> m_pfilemanager;
+    std::string m_path;
+    bool m_fCreatedTempFolder = false;
+
+public:
+    RocksDBStorageFactory(const char *dbfile, int dbnum, const char *rgchConfig, size_t cchConfig);
+    ~RocksDBStorageFactory();
+
+    virtual IStorage *create(int db, key_load_iterator iter, void *privdata) override;
+    virtual const char *name() const override;
+
+    virtual size_t totalDiskspaceUsed() const override;
+
+    virtual bool FSlow() const override { return true; }
+
+    virtual size_t filedsRequired() const override;
+    std::string getTempFolder();
+
+private:
+    void setVersion(rocksdb::ColumnFamilyHandle*);
+};
---
+++ tests/integration/replication-fast.tcl
@@ -0,0 +1,34 @@
+proc prepare_value {size} {
+    set _v "c"
+    for {set i 1} {$i < $size} {incr i} {
+        append _v 0
+    }
+    return $_v
+}
+
+start_server {tags {"replication-fast"} overrides {storage-provider {flash ./rocks.db.master}}} {
+    set slave [srv 0 client]
+    set slave_host [srv 0 host]
+    set slave_port [srv 0 port]
+    start_server {tags {} overrides {storage-provider {flash ./rocks.db.replica}}} {
+        set master [srv 0 client]
+        set master_host [srv 0 host]
+        set master_port [srv 0 port]
+
+        test "fast replication with large value" {
+            set _v [prepare_value [expr 64*1024*1024]]
+            # $master set key $_v 
+
+            $slave replicaof $master_host $master_port
+			wait_for_condition 50 300 {
+                [lindex [$slave role] 0] eq {slave} &&
+                [string match {*master_link_status:up*} [$slave info replication]]
+            } else {
+                fail "Can't turn the instance into a replica"
+            }
+
+            assert_equal [$slave debug digest] [$master debug digest]
+            $slave replicaof no one
+        }
+    }
+}

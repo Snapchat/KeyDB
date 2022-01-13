b10b414ed53d0e2c7ae7f3c48ca2dfe8abaa99c0
christianEQ <christian@eqalpha.com>
2022-01-13 00:07:03 +0000

    bump version
    Former-commit-id: 35dc4df1df72ac0ff8224ad664426f26bd4c8868

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-#define KEYDB_REAL_VERSION "6.2.1"
-#define KEYDB_VERSION_NUM 0x00060201
+#define KEYDB_REAL_VERSION "6.2.2"
+#define KEYDB_VERSION_NUM 0x00060202
 extern const char *KEYDB_SET_VERSION;   // Unlike real version, this can be overriden by the config
 

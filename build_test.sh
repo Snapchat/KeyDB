24c6b65c6f9a48d3592a5783b3d9574addecc572
zliang <zliang@snapchat.com>
2022-04-22 16:46:41 -0600

    fix the Machamp build

--- build_test.sh
+++ build_test.sh
@@ -1,11 +1,11 @@
 # make the build
+git submodule init && git submodule update
 make BUILD_TLS=yes -j2 KEYDB_CFLAGS='-Werror' KEYDB_CXXFLAGS='-Werror'
 
 # gen-cert
 ./utils/gen-test-certs.sh
 
 # test-tls
-apt-get -y install tcl tcl-tls
 ./runtest --clients 2 --verbose --tls
 
 # cluster-test

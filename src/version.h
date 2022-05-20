a9511e7d088af668f7d038374aceabea4736dd23
John Sully <john@csquare.ca>
2022-05-20 20:42:31 +0000

    Version bump

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
-#define KEYDB_REAL_VERSION "6.3.0"
-#define KEYDB_VERSION_NUM 0x00060300
+#define KEYDB_REAL_VERSION "6.3.1"
+#define KEYDB_VERSION_NUM 0x00060301
 extern const char *KEYDB_SET_VERSION;   // Unlike real version, this can be overriden by the config
 
 enum VersionCompareResult

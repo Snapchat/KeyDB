2ca1987862a57ae1bb1998a329be933bc22c88f3
Malavan Sotheeswaran <msotheeswaran@snapchat.com>
2022-12-12 11:27:15 -0800

    remove more internal ci/publishing

--- machamp_scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# make the build
-git submodule init && git submodule update
-make BUILD_TLS=yes -j$(nproc) KEYDB_CFLAGS='-Werror' KEYDB_CXXFLAGS='-Werror'
-
-# gen-cert
-./utils/gen-test-certs.sh

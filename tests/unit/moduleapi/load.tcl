cc2aa192cf4f981285f87fa2c7686dd63c22c0bf
Malavan Sotheeswaran <msotheeswaran@snapchat.com>
2023-01-07 01:33:54 -0800

    help

--- tests/unit/moduleapi/load.tcl
+++ tests/unit/moduleapi/load.tcl
@@ -5,7 +5,7 @@ if {$::flash_enabled} {
         r set bar foo
         r set foobar barfoo
     }
-    start_server {tags {"modules"} overrides {storage-provider {flash ./rocks.db.master} databases 256 loadmodule tests/modules/load.so}} {
+    start_server {tags {"modules"} overrides {storage-provider {flash ./rocks.db.master} databases 256 loadmodule tests/modules/load.so module-notify-flash-load yes}} {
         test "Module is notified of keys loaded from flash" {
             assert_equal [r load.count] [r dbsize]
         }

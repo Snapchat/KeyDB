40c4887a835fc75374f76c532aff4307aee7bb5c
Malavan Sotheeswaran <msotheeswaran@snapchat.com>
2023-01-09 18:16:22 -0800

    fix test

--- tests/unit/moduleapi/load.tcl
+++ tests/unit/moduleapi/load.tcl
@@ -1,14 +1,14 @@
 set testmodule [file normalize tests/modules/load.so]
 
 if {$::flash_enabled} {
-    start_server {tags {"modules"} overrides {storage-provider {flash ./rocks.db.master} databases 256}} {
-        r flushall
-        r set foo bar
-        r set bar foo
-        r set foobar barfoo
-    }
     start_server [list tags [list "modules"] overrides [list storage-provider {flash ./rocks.db.master} databases 256 loadmodule $testmodule]] {
         test "Module is notified of keys loaded from flash" {
+            r flushall
+            r set foo bar
+            r set bar foo
+            r set foobar barfoo
+            assert_equal [r load.count] 0
+            r debug reload
             assert_equal [r load.count] [r dbsize]
         }
     }

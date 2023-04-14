30d058f160703e932858fec4d74ec43c451e88fc
Malavan Sotheeswaran <msotheeswaran@snapchat.com>
2023-04-14 09:50:45 -0700

    bump version

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
-#define KEYDB_REAL_VERSION "6.3.2"
-#define KEYDB_VERSION_NUM 0x00060302
+#define KEYDB_REAL_VERSION "6.3.3"
+#define KEYDB_VERSION_NUM 0x00060303
 extern const char *KEYDB_SET_VERSION;   // Unlike real version, this can be overriden by the config
 
 enum VersionCompareResult
